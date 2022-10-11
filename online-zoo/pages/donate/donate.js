0b729f5a7bd31f06f20eedf58eb777f6577f79a0
ULADZISLAVA <vladislava96.96@mail.ru>
2022-10-11 22:40:32 +0300

    feat: fix: burger menu blackout in donate page

--- online-zoo/pages/donate/donate.js
+++ online-zoo/pages/donate/donate.js
@@ -3,6 +3,6 @@ import BurgerMenu from "../../assets/js/burger-menu.js";
 const burgerMenu = document.querySelector('.burger-menu');
 const openMenuBtn = document.querySelector('.header__burger-menu-btn');
 const closeMenuBtn = document.querySelector('.burger-menu__close-btn');
-const backgroundBurger = document.querySelector('.burger-menu__background');
+const backgroundBurger = document.querySelector('.menu-and-popup__background');
 
 new BurgerMenu(burgerMenu, openMenuBtn, closeMenuBtn, backgroundBurger)
